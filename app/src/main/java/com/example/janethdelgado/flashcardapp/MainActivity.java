29d63ac74d33303ad99af371328a4906a1a4036a
jdelgado111 <jdm41091@gmail.com>
2018-10-13 17:06:36 -0700

    Added all of the optional tasks.

--- app/src/main/java/com/example/janethdelgado/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/janethdelgado/flashcardapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.janethdelgado.flashcardapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isShowingOptions = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,59 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
             }
         });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.flashcard_option1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_option1).setBackgroundColor(getResources().getColor(R.color.wrongAnswer));
+                findViewById(R.id.flashcard_option2).setBackgroundColor(getResources().getColor(R.color.rightAnswer));
+            }
+        });
+
+        findViewById(R.id.flashcard_option2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_option2).setBackgroundColor(getResources().getColor(R.color.rightAnswer));
+            }
+        });
+
+        findViewById(R.id.flashcard_option3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_option3).setBackgroundColor(getResources().getColor(R.color.wrongAnswer));
+                findViewById(R.id.flashcard_option2).setBackgroundColor(getResources().getColor(R.color.rightAnswer));
+            }
+        });
+
+        findViewById(R.id.toggle_options_visibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isShowingOptions == true)
+                {
+                    ((ImageView) findViewById(R.id.toggle_options_visibility)).setImageResource(R.drawable.view_icon);
+                    findViewById(R.id.flashcard_option1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_option2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_option3).setVisibility(View.INVISIBLE);
+                    isShowingOptions = false;
+                }
+                else
+                {
+                    ((ImageView) findViewById(R.id.toggle_options_visibility)).setImageResource(R.drawable.hide_icon);
+                    findViewById(R.id.flashcard_option1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_option2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_option3).setVisibility(View.VISIBLE);
+                    isShowingOptions = true;
+                }
+            }
+        });
+
     }
 }
