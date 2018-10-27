00077c395806138e1195a062c7d706011cdc66c4
jdelgado111 <jdm41091@gmail.com>
2018-10-27 16:13:10 -0700

    Implemented optional tasks (noted in README)

--- app/src/main/java/com/example/janethdelgado/flashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/janethdelgado/flashcardapp/AddCardActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -12,6 +13,12 @@ public class AddCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        String question = getIntent().getStringExtra("question");
+        String answer = getIntent().getStringExtra("answer");
+
+        ((EditText) findViewById(R.id.questionText)).setText(question);
+        ((EditText) findViewById(R.id.answerText)).setText(answer);
 
         findViewById(R.id.cancelCard).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,10 +32,18 @@ public class AddCardActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String question = ((EditText) findViewById(R.id.questionText)).getText().toString();
                 String answer = ((EditText) findViewById(R.id.answerText)).getText().toString();
+                String option1 = ((EditText) findViewById(R.id.option1Text)).getText().toString();
+                String option2 = ((EditText) findViewById(R.id.option2Text)).getText().toString();
+                if (question.matches("") || answer.matches("")) {
+                    Toast.makeText(getApplicationContext(), "Must enter a question and answer!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 Intent data = new Intent(); // create a new Intent to put our data
                 data.putExtra("question", question); // puts one string into the Intent (key, string)
                 data.putExtra("answer", answer); // puts another string into the Intent (key, string)
+                data.putExtra("option1", option1);
+                data.putExtra("option2", option2);
                 setResult(RESULT_OK, data); // set result code and bundle data for response
                 finish(); // closes this activity and pass data to the original activity that launched this activity
             }
--- app/src/main/java/com/example/janethdelgado/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/janethdelgado/flashcardapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.janethdelgado.flashcardapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -84,16 +85,39 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivityForResult(intent, 100);
             }
         });
+
+        findViewById(R.id.editCard).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question = ((TextView)findViewById(R.id.flashcard_question)).getText().toString();
+                String answer = ((TextView)findViewById(R.id.flashcard_answer)).getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                intent.putExtra("question", question);
+                intent.putExtra("answer", answer);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+        if (requestCode == 100 && resultCode == RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
             String question = data.getExtras().getString("question"); // 'string1' needs to match the key we used when we put the string in the Intent
             String answer = data.getExtras().getString("answer");
+            String option1 = data.getExtras().getString("option1");
+            String option2 = data.getExtras().getString("option2");
 
             ((TextView)findViewById(R.id.flashcard_question)).setText(question);
             ((TextView)findViewById(R.id.flashcard_answer)).setText(answer);
+
+            if (!option1.matches("") && !option2.matches("")) {
+                ((TextView)findViewById(R.id.flashcard_option1)).setText(option1);
+                ((TextView)findViewById(R.id.flashcard_option2)).setText(answer);
+                ((TextView)findViewById(R.id.flashcard_option3)).setText(option2);
+            }
+
+            Snackbar.make(findViewById(android.R.id.content), "Card successfully created", Snackbar.LENGTH_LONG).show();
         }
     }
 
